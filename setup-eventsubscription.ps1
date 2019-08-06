a6574c9f3bc15704c87a3ce8e85c877b8d2fbcaf
ayuina <ayumu.inaba+git@gmail.com>
2019-08-06 23:42:24 +0900

    event subscription

---
+++ setup-eventsubscription.ps1
@@ -0,0 +1,20 @@
+$rgname = "ayuina-devops-rg"
+$funcappname = "ayuina-devops-func"
+$funcname = "thumbnail"
+$strname = "ayuinafuncstr"
+$container = "images"
+
+az functionapp show -g $rgname -n $funcappname | ConvertFrom-Json | Set-Variable targetFunc
+$manageurl = "{0}/host/default/listKeys?api-version=2018-02-01" -f $targetFunc.id
+az rest --method post --uri $manageurl | ConvertFrom-Json | Set-Variable funcKeys
+
+$endpoint = ("https://{0}.azurewebsites.net/runtime/webhooks/EventGrid?functionName={1}&code={2}" `
+                -f $funcappname, $funcname, $funcKeys.systemKeys.eventgrid_extension )
+Write-Host $endpoint
+
+az eventgrid resource event-subscription create -g $rgname `
+    --provider-namespace Microsoft.Storage --resource-type storageAccounts `
+    --resource-name $strname --name "image-subscription"  `
+    --included-event-types Microsoft.Storage.BlobCreated `
+    --subject-begins-with "/blobServices/default/containers/$container/blobs/" `
+    --endpoint $endpoint
